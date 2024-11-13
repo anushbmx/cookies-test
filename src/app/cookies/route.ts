1c1b3b37f5800e96e470c9efc4b9813841dbe851
Anush A <anush@techstream.org>
2024-11-13 19:12:37 +0530

    Added request url

--- src/app/cookies/route.ts
+++ src/app/cookies/route.ts
@@ -2,6 +2,7 @@
 import { setSignedCookies } from "./utls.ts";
 
 export async function GET(request: Request) {
+  console.log(`===== Request URL : ${request.url}`)
   await setSignedCookies(request.url);
   return new Response("Cookie set", {
     status: 200,
